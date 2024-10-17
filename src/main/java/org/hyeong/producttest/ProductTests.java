9ea6ac06477dd24b31a646c929efa9a42d98d466
DSL0680 <ekthf988172@gmail.com>
2024-10-17 15:26:33 +0900

    feat : tests

--- src/main/java/org/hyeong/producttest/ProductTests.java
+++ src/main/java/org/hyeong/producttest/ProductTests.java
@@ -1,4 +1,12 @@
 package org.hyeong.producttest;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class ProductTests {
+
+    public ProductTests() {
+
+        log.info("testtesttest");
+    }
 }
