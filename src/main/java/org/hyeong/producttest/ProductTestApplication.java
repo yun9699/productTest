726ed8e29d93cd8cf1d72d6412ad01b28ff92975
yun9699 <cha23215@daum.net>
2024-10-17 15:25:05 +0900

    test!

--- src/main/java/org/hyeong/producttest/ProductTestApplication.java
+++ src/main/java/org/hyeong/producttest/ProductTestApplication.java
@@ -2,6 +2,9 @@ package org.hyeong.producttest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ProductTestApplication {
@@ -10,4 +13,15 @@ public class ProductTestApplication {
         SpringApplication.run(ProductTestApplication.class, args);
     }
 
+    // 새로운 REST 컨트롤러를 추가해보자
+    @RestController
+    @RequestMapping("/product")
+    public class ProductController {
+
+        // 간단한 GET 요청에 대한 응답을 반환
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello, this is your product API!";
+        }
+    }
 }
