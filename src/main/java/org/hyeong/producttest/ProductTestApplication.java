0476df3eddec9013e5fd5bb8d08ce678c3ad4bb0
jhyeong <hyeongcode@gmail.com>
2024-10-17 15:21:39 +0900

    pruducttest/alwoerury, ProductTestApplication

--- src/main/java/org/hyeong/producttest/ProductTestApplication.java
+++ src/main/java/org/hyeong/producttest/ProductTestApplication.java
@@ -1,13 +1,16 @@
 package org.hyeong.producttest;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class ProductTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductTestApplication.class, args);
     }
+
 
 }
