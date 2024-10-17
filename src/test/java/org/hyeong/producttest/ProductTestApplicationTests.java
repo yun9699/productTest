2c64a329de2af5f099f60e011c7f7ce07934d87a
jhyeong <hyeongcode@gmail.com>
2024-10-17 14:33:09 +0900

    init

---
+++ src/test/java/org/hyeong/producttest/ProductTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeong.producttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
